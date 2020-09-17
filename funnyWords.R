4ee55aca04333706c364c59c5d79e8e51aee0e3f
Diren Senger <diren@diren.de>
2020-09-17 10:50:51 +0200

    last presi version, updated funnyWord.R

--- funnyWords.R
+++ funnyWords.R
@@ -1,12 +1,16 @@
-funny.words <- function(s = "summer", count = 3){
-  s = "summer"
+own.substr <- function(s,i){
+  return(substr(s, i, i))
+}
+
+funny.words <- function(my.word = "summer", count = 3){
   s1 <- ""
   for(i in 1:2){
     for(i in 1:count){
-      print(substr(s, i, i))
-      s1 <- paste(s1, substr(s, i, i), sep = "")
+      print(substr(my.word, i, i))
+      sub <- own.substr(my.word,i)
+      s1 <- paste(s1, sub, sep = "")
     }
   }
   return(s1)
 }
-funny.words()
+funny.words("banana")
